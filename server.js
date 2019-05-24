fd0e5476dda96d34f022171a93f040000b79116a
WHBJ <bevansrealtors@gmail.com>
2019-05-24 17:47:03 -0500

    heroku push

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://bill:0062129306191992Kate@ds261296.mlab.com:61296/heroku_qlzkvhs5");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://hamilton:00621293Kate@ds261296.mlab.com:61296/heroku_qlzkvhs5");
 
 // Start the API server
 app.listen(PORT, function() {
